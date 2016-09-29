511475e511d74cbbe0b42252e4a7fca17b6b2bb5
agb91 <andrea.damio1991@hotmail.it>
2016-09-29 23:34:23 +0200

    the back-end works, the front-end simply doesn't exist

--- app/src/main/java/com/example/andrea/ieeesensors/MainActivity.java
+++ app/src/main/java/com/example/andrea/ieeesensors/MainActivity.java
@@ -1,75 +1,48 @@
 package com.example.andrea.ieeesensors;
+
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.net.Uri;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.os.Build;
-
-import org.w3c.dom.Text;
-
 import java.util.List;
 import java.util.Vector;
 
-/**
- *
- */
+// implements SensorEventListener is to use the OnChangeEvents linked to the sensors
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
-    private Sensor mLight;
     private TextView monitor;
-    private List<Sensor> listAllSensors;
-    private Vector<String> allTheSensorsToPrint = new Vector<String>();
+    private List<Sensor> listAllSensors;// in this list all the sensors
+    private Vector<String> allTheSensorsToPrint = new Vector<String>();// in this list all the string to print (each string related to a sensor)
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        monitor = (TextView) findViewById(R.id.monitor);
+        monitor = (TextView) findViewById(R.id.monitor);// just a textview where to write the results
         monitor.setText("");
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        listAllSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        listAllSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL); // in this list all the sensors
 
-        for( int i = 0; i < 100 ; i++ )
+        //maybe the following 4 rows are useless..
+        for( int i = 0; i < 50 ; i++ ) //in this moment i wanna 50 rows to write something (I don't know what)
         {
-            allTheSensorsToPrint.add("-");
+            allTheSensorsToPrint.add("-");// I don't like empty strings
         }
 
-        for (Sensor e : listAllSensors)
+        for (Sensor e : listAllSensors)// for all the existing sensors, taken as "e"
         {
+            //register this sensor, so if it changes onChangeSensor is called
             mSensorManager.registerListener(this, e, mSensorManager.SENSOR_DELAY_NORMAL);
         }
-
-        //mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        //mCompass = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
     }
+
 
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -77,24 +50,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
+    public final void onSensorChanged(SensorEvent event) {//when some (at least 1) sensor changes
         synchronized (this) {
-            try
+            try// sometimes it crashes..
             {
                 String v = "";
-                for(int i = 0; i < event.values.length; i++)
+                for(int i = 0; i < event.values.length; i++)// for all the values of the sensor (sometimes sensors have 3 value, sometimes only 1)
                 {
-                    v+=event.values[i] + " ; ";
+                    v+=event.values[i] + " ; ";//print them separated by semicolon
                 }
-                // Do something with this sensor value.
-                //tv1 = (TextView) findViewById(R.id.text1);
+                //create a string to show the name and the value of the sensor
                 String toPrint = "I'm : " + event.sensor.getName() + " ==> " + String.valueOf(v) + "  ; ";
 
+                //put this string in the vector, each sensor in a different row (based on the sensor's type)
                 allTheSensorsToPrint.set(event.sensor.getType(),  toPrint );
                 monitor.setText("");
-                for( int i = 0; i < allTheSensorsToPrint.size() ; i++ )
+                for( int i = 0; i < allTheSensorsToPrint.size() ; i++ )// every time something changes reprint all the sensors' line
                 {
                     monitor.append( allTheSensorsToPrint.get( i ) + " \n " );
                 }
@@ -102,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             catch (Exception e)
             {
                 monitor.setText("");
-                monitor.append( "black death");
+                monitor.append( "black death");//shit happens
             }
 
         }
@@ -127,73 +98,3 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mSensorManager.unregisterListener(this);
     }
 }
-   /* public static String BARCODE="",MESSAGE="";
-    SensorManager smm;
-    List<Sensor> list;
-    //barcode
-    //private int requestCodeGetQR = 1;
-    //private String barcode="",message="";
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    /**
-     *
-     * @param view
-     */
-/*    public void getData(View view) {
-        smm = (SensorManager) getSystemService(SENSOR_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
-            list = smm.getSensorList(Sensor.TYPE_ALL);
-        }
-        TextView monitor = (TextView) findViewById(R.id.monitor);
-        monitor.setText("");
-
-        for (Sensor e : list){
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
-                monitor.append(" \n name: " + e.getName());
-            }
-
-        }
-    }
-
-  /*  /**
-     *
-     * @param view
-     */
-/*    public void sendEmail(View view){
-        Intent intent= new Intent(Intent.ACTION_SENDTO);
-        TextView monitor = (TextView) findViewById(R.id.monitor);
-        intent.setType("plain/text");
-        intent.setData(Uri.parse("mailto:"));
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Detect Sensors on my phone");
-        intent.putExtra(Intent.EXTRA_TEXT, monitor.getText());
-        intent.putExtra(Intent.EXTRA_EMAIL, "test@gmail.com");
-        if(intent.resolveActivity(getPackageManager())!=null) {
-            startActivity(intent);
-            message(getApplicationContext(),"Apertura email");
-        }
-        else
-            message(getApplicationContext(),"Non è possibile inviare alcuna email");
-    }
-*/
-
-    /*@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == requestCodeGetQR && resultCode == RESULT_OK && data != null) {
-            barcode = data.getStringExtra(BARCODE);
-            message = data.getStringExtra(MESSAGE);
-            barcode();
-        }
-    }*/
-    /*private void message(Context context, String message){
-        Log.i(context.getPackageName(), "message: message="+message);
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-    }
-
-}
-*/
